ba7ee5887cd700a8628bb315cadf949039b40f22
caisarioihsandev <iscaisario@gmail.com>
2024-09-19 16:42:00 +0700

    Error Handling

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 const app = express();
 const port = 3000;
@@ -21,60 +23,77 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
-  // Show all campgrounds data from database
-  try {
-    sql = `SELECT * FROM campgrounds;`;
-    const campgrounds = await new Promise((resolve, reject) => {
-      db.all(sql, (err, rows) => {
-        if (err) {
-          return reject(err);
-        }
-        if (rows.length === 0) {
-          console.log('There is no camp!!');
-        }
+app.get(
+  '/campgrounds',
+  catchAsync(async (req, res) => {
+    // Show all campgrounds data from database
+    try {
+      sql = `SELECT * FROM campgrounds;`;
+      const campgrounds = await new Promise((resolve, reject) => {
+        db.all(sql, (err, rows) => {
+          if (err) {
+            return reject(err);
+          }
+          if (rows.length === 0) {
+            console.log('There is no camp!!');
+          }
 
-        resolve(rows);
+          resolve(rows);
+        });
       });
-    });
-    res.render('campgrounds/index', { campgrounds });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+      res.render('campgrounds/index', { campgrounds });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  })
+);
 
-// Add New Campground
+// Add New Campground page
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
 
 // To insert new campground
-app.post('/campgrounds', async (req, res) => {
-  try {
+app.post(
+  '/campgrounds',
+  catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground Data', 400);
+
     sql = `INSERT INTO campgrounds (title, location, image, price, description)
       VALUES (?, ?, ?, ?, ?)`;
     const { title, location, image, price, description } = req.body.campground;
+    const priceNumber = Number(price);
+
+    if (!priceNumber) {
+      throw new ExpressError('Invalid Campground Data', 400);
+    } else if (priceNumber < 0) {
+      throw new ExpressError('Price must be greater than 0', 400);
+    }
 
     const addedCampground = await new Promise((resolve, reject) => {
-      db.run(sql, [title, location, image, price, description], function (err) {
-        if (err) {
-          return reject(err);
+      db.run(
+        sql,
+        [title, location, image, priceNumber, description],
+        function (err) {
+          if (err) {
+            return reject(err);
+          }
+          // to grab the id of insertion data
+          resolve({ id: this.lastID });
         }
-        // to grab the id of insertion data
-        resolve({ id: this.lastID });
-      });
+      );
     });
 
     res.redirect(`/campgrounds/${addedCampground.id}`);
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
-});
+  })
+);
 
 // For single campground page
-app.get('/campgrounds/:id', async (req, res) => {
-  try {
+app.get(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
     const campId = req.params.id;
     sql = `SELECT * FROM campgrounds WHERE id=?`;
     const campground = await new Promise((resolve, reject) => {
@@ -82,23 +101,18 @@ app.get('/campgrounds/:id', async (req, res) => {
         if (err) {
           return reject(err);
         }
-        if (!rows) {
-          console.log('There is no camp!!');
-        }
 
         resolve(rows);
       });
     });
     res.render('campgrounds/show', { campground });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+  })
+);
 
 // Edit form
-app.get('/campgrounds/:id/edit', async (req, res) => {
-  try {
+app.get(
+  '/campgrounds/:id/edit',
+  catchAsync(async (req, res) => {
     const campId = req.params.id;
     sql = `SELECT * FROM campgrounds WHERE id=?`;
     const campground = await new Promise((resolve, reject) => {
@@ -114,22 +128,32 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
       });
     });
     res.render('campgrounds/edit', { campground });
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
-});
+  })
+);
 
 // edit campground
-app.put('/campgrounds/:id', async (req, res) => {
-  try {
+app.put(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground Data', 400);
     const campId = req.params.id;
     const { title, location, image, price, description } = req.body.campground;
+    const priceNumber = Number(price);
+
+    if (!priceNumber) {
+      throw new ExpressError('Invalid Campground Data', 400);
+    } else if (priceNumber < 0) {
+      throw new ExpressError('Price must be greater than 0', 400);
+    }
+
     sql = `UPDATE campgrounds SET title = ?, location = ?, image = ?, price = ?, description = ? 
         WHERE id = ?`;
+
     const editedCampground = await new Promise((resolve, reject) => {
       db.run(
         sql,
-        [title, location, image, price, description, campId],
+        [title, location, image, priceNumber, description, campId],
         function (err) {
           if (err) {
             return reject(err);
@@ -145,14 +169,13 @@ app.put('/campgrounds/:id', async (req, res) => {
     }
 
     res.redirect(`/campgrounds/${campId}`);
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
-});
+  })
+);
 
 // delete campground
-app.delete('/campgrounds/:id', async (req, res) => {
-  try {
+app.delete(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
     const campId = req.params.id;
     sql = `DELETE FROM campgrounds WHERE id = ?`;
 
@@ -171,9 +194,17 @@ app.delete('/campgrounds/:id', async (req, res) => {
     }
 
     res.redirect('/campgrounds');
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
+  })
+);
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found'), 404);
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Something went wrong!';
+  res.status(statusCode).render('error', { err });
 });
 
 app.listen(port, () => {
